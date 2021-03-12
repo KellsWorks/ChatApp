2abac43a7871730028adf23435bf73b59a943f49
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-03-12 13:46:02 +0200

    Fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,40 @@
+import AppLoading from 'expo-app-loading';
+
+import * as Font from 'expo-font';
+
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+
+import React, { useState } from 'react';
+
 import { StyleSheet, Text, View } from 'react-native';
 
+const LoadFonts = () =>{
+  return Font.loadAsync({
+    'Font-normal': require('./assets/fonts/Montserrat-Regular.ttf'),
+    'Font-medium': require('./assets/fonts/Montserrat-Medium.ttf'),
+    'Font-bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+  });
+}
+
 export default function App() {
+
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if(!fontLoaded){
+
+    return(
+      <AppLoading
+        startAsync={LoadFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err: any) => console.error(err)}
+      >
+      </AppLoading>
+    ); 
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontFamily: 'Font-medium' }}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,3 +48,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+    function useCachedResources() {
+      throw new Error('Function not implemented.');
+    }
+

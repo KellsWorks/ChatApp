056abccd6518b9be05662768a509d46f8c3217ba
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-03-13 18:38:51 +0200

    Update Register.tsx

--- screens/auth/Register.tsx
+++ screens/auth/Register.tsx
@@ -1,14 +1,51 @@
+import { Feather } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native';
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-export class Register extends Component {
-    render() {
-        return (
-            <View style={{ flex: 1, backgroundColor: 'white'}}>
-                <Text> Let's code some data in here </Text>
+const Register = () => {
+
+    const navigation = useNavigation();
+    
+    return (
+        <SafeAreaView style={styles.safeArea}>
+            <View>
+            <View style={styles.container}>
+                <Feather name="arrow-left" size={25} style={{ marginLeft: 20}} color="gray" onPress={()=> navigation.goBack()}/>
+                <Text style={styles.title}>Get started!</Text>
             </View>
-        )
-    }
+            {/* <View>
+                <Text>
+                    Fill out all the required fields
+                </Text>
+            </View> */}
+            </View>
+        </SafeAreaView>
+    )
 }
 
 export default Register
+
+const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1, backgroundColor: 'white',
+        flexDirection: 'column',
+    },
+    container: {
+        width: '100%',
+        height: 30,
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        flexDirection: 'row',
+        paddingTop: 40,
+        alignContent: 'center'
+    },
+    title: {
+        fontFamily: 'Font-bold',
+        fontSize: 30,
+        color: 'gray',
+        marginLeft: 40,
+        marginTop: -10,
+    }
+})
